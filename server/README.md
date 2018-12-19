b161690dc079659157bbad1b9190255e6ee4dc67
User <user@Users-MacBook-Pro-3.local>
2018-12-19 15:37:17 -0800

    adds initial project files

---
+++ server/README.md
@@ -0,0 +1,5 @@
+Create a new server using  Express 
+Log all incoming requests with the morgan logging 
+Serve the Top Spots project from the web server
+Serve the Top Spots data from the web server
+Update  Top Spots front end with new back end
--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
 // import files and packages up here
 
+const express = require('express');
 
-// create your express server below
-var app;
+const data = require('./data.json');
+
+const morgan = require('morgan')
+
+
+const app = express();
+
+
+app.use(morgan('dev'));
+
+
+const port = 3000;
 
 // add your routes and middleware below
 
+app.get('/', function(req,res) {
+    res.status(200).send('hello world');
+});
+
+app.get('/data', function(req, res,){
+ res.status(200).json(data);
+ 
+});
+
+
 // finally export the express application
+
 module.exports = app;
